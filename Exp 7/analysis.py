a078a5f95f1b2fb00aa921844e460a142b45720c
Marco Canteri <canterimarco@gmail.com>
2017-11-12 16:52:00 +0100

    update

--- Exp 7/analysis.py
+++ Exp 7/analysis.py
@@ -34,6 +34,7 @@ plt.plot(np.linspace(0,350,1300),cos_fit(np.linspace(0,350,1300),*params),label 
 plt.legend()
 
 #SHG power
+#function of theta
 file = np.arange(0,36)
 mean = np.zeros(len(file))
 error = np.zeros(len(file))
@@ -46,12 +47,12 @@ angles = np.arange(0,360,10)
 
 plt.figure()
 plt.errorbar(angles,mean,yerr=error,xerr = np.full_like(error, 2/np.sqrt(12)),fmt='.-',label="Experimental points" )
-plt.xlabel("Angle [°]")
+plt.xlabel(r"Angle $\phi$ [°]")
 plt.ylabel("Voltage [V]")
 plt.legend()
 
 
-
+#close up of a maxium
 file = np.arange(36,53)
 mean = np.zeros(len(file))
 error = np.zeros(len(file))
@@ -69,7 +70,10 @@ plt.ylabel("Voltage [V]")
 
 
 
+def adjust_angle(x):
+	return 0.8666666666666667*x-15
 
+#as a function of phi
 file = np.arange(53,62)
 mean = np.zeros(len(file))
 error = np.zeros(len(file))
@@ -79,11 +83,46 @@ for i in range(len(file)):
 	error[i] = np.std(v)/np.sqrt(len(v))
 
 angles = np.concatenate([np.arange(35,65,5),np.arange(25,10,-5)])
+angles = adjust_angle(angles)
 
+#evil sorting
+indexes = np.argsort(angles,kind = "mergesort")
+print(indexes)
+print(angles)
+a = np.zeros_like(angles)
+b = np.zeros_like(angles)
+c =  np.zeros_like(angles)
+j = 0
+for i in indexes:
+	a[j] = angles[i]
+	b[j] = mean[i]
+	c[j] = error[i]
+	j+=1
+plt.figure()
+plt.errorbar(a,b,yerr=c,xerr = np.full_like(error,1/np.sqrt(12)),fmt='.-',label="Experimental points" )
+#plt.errorbar(adjust_angle(angles),mean,yerr=error,xerr = np.full_like(error,1/np.sqrt(12)),fmt='.-',label="Experimental points" )
+plt.xlabel(r"Angle $\theta$ [°]")
+plt.ylabel("Voltage [V]")
+plt.legend(loc = 3)
+
+
+
+
+
+file = np.arange(62,73)
+mean = np.zeros(len(file))
+error = np.zeros(len(file))
+for i in range(len(file)):
+	t,v = read_oscilloscope_data("data/TEK00"+str(file[i]).zfill(2)+".CSV")
+	mean[i] = np.mean(v)
+	error[i] = np.std(v)/np.sqrt(len(v))
+
+angles = np.arange(130,240,10)
 plt.figure()
 plt.errorbar(angles,mean,yerr=error,xerr = np.full_like(error,2/np.sqrt(12)),fmt='.' )
 plt.xlabel("Angle [°]")
 plt.ylabel("Voltage [V]")
+
 plt.show()
 
 #params, pcov = curve_fit(fit_sine, angle, data, sigma=dataerr)
